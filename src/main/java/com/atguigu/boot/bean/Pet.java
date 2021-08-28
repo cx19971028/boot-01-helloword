6339afb909e8011ecd587efd2d80e950c93343f1
陈新 <757617711@qq.com>
2021-08-28 17:06:34 +0800

    hot-fix commit3

--- src/main/java/com/atguigu/boot/bean/Pet.java
+++ src/main/java/com/atguigu/boot/bean/Pet.java
@@ -6,6 +6,7 @@ public class Pet {
     private Integer age;
     private String addr;
     private String gir4;
+    private String hot_fix;
 
     public Pet(String name) {
         this.name = name;
